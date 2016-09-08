edd6071ab0031e7a1da1698df83a81ee35436d33
Navendu Agarwal <navendu.agarwal@gmail.com>
2016-09-08 09:27:10 +0530

    updated format

--- app/src/main/java/com/sparshik/yogicapple/utils/DateUtils.java
+++ app/src/main/java/com/sparshik/yogicapple/utils/DateUtils.java
@@ -6,6 +6,7 @@ import android.text.format.Time;
 import com.sparshik.yogicapple.R;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * Utility to format dates and timestamps
@@ -23,7 +24,7 @@ public class DateUtils {
         // If the date we're building the String for is today's date, the format
         // is "Today at TimeStamp"
 
-        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
         String formattedTime = timeFormat.format(dateInMillis);
         int formatId = R.string.format_full_friendly_timestamp;
 
@@ -34,11 +35,11 @@ public class DateUtils {
             String yesterday = context.getString(R.string.yesterday);
             return context.getString(formatId, yesterday, formattedTime);
         } else if (julianDay > currentJulianDay - 7) {
-            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
             String formattedDay = "Last " + dayFormat.format(dateInMillis);
             return context.getString(formatId, formattedDay, formattedTime);
         } else {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd", Locale.US);
             String formattedDate = dateFormat.format(dateInMillis);
             return context.getString(formatId, formattedDate, formattedTime);
         }
